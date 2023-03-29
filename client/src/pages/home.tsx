dfe5f43101866e5b21f357596796c9d108a79f12
Isai Martinez <isai.martinez@yeico.com>
2023-03-29 17:42:38 -0600

    Home Ready

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -14,27 +14,31 @@ const home = () => {
           title="Properties for Sale"
           value={684}
           series={[75, 25]}
-          colors={['#475be8', '#e4e8ef']}
+          colors={['#275be8', '#c4e8ef']}
         />
         <PieChart 
           title="Properties for Rent"
           value={550}
           series={[60, 40]}
-          colors={['#475ae8', '#e4e8ef']}
+          colors={['#275be8', '#c4e8ef']}
         />
         <PieChart 
           title="Total Customers"
           value={5684}
           series={[75, 25]}
-          colors={['#275be8', '#e4e8ef']}
+          colors={['#275be8', '#c4e8ef']}
         />
         <PieChart 
           title="Properties for Cities"
           value={555}
           series={[75, 25]}
-          colors={['#475be8', '#e4e8ef']}
+          colors={['#275be8', '#c4e8ef']}
         />
       </Box>
+      <Stack mt="25px" width="100%" direction={{xs: 'column', lg: 'row'}} gap={4}>
+        <TotalRevenue />
+        <PropertyReferrals />
+      </Stack>
     </Box>
   )
 }
